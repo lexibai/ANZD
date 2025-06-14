68b6a33409f5ce39f4816a97a6cee00d30e0f632
花天酒地唐斜阳 <thle522@foxmail.com>
2025-06-14 20:46:42 +0800

    feat(Skill): 移除 fireSkill 并优化 SpeedUpSkill效果
    - 删除了 fireSkill.cs 及其元数据文件- 在 SpeedUpSkill 中添加了创建拖影效果的 CreateShadow 方法
    - 引入了 DOTween 库并添加了相关设置文件

--- Assets/Scripts/Skill/Impl/Move/SpeedUpSkill.cs
+++ Assets/Scripts/Skill/Impl/Move/SpeedUpSkill.cs
@@ -2,6 +2,7 @@
 using DefaultNamespace;
 using DefaultNamespace.Buff;
 using DefaultNamespace.Buff.Command;
+using DG.Tweening;
 using QFramework;
 using UnityEngine;
 
@@ -17,7 +18,7 @@ namespace Model.Skill.Impl.Move
             {
                 id = Random.Range(-1000000000, 0),
                 name = null,
-                priority = 100,
+                priority = 20,
                 duration = 0.1f,
                 tickTime = 0,
                 canPermanent = false,
@@ -37,9 +38,43 @@ namespace Model.Skill.Impl.Move
             
             this.SendCommand<AddBuffCommand<BaseBuffObj>>(new AddBuffCommand<BaseBuffObj>(userObj, buffData));
             
+            //创建拖影
+            var sprites = userObj.GetComponentsInChildren<SpriteRenderer>();
+
+            ActionKit.Repeat(5)
+                .Callback(() => { CreateShadow(sprites); })
+                .Delay(0.02f)
+                .Start(userObj);
+
+
+
         }
-        
 
+        private static void CreateShadow(SpriteRenderer[] sprites)
+        {
+            foreach (var sr in sprites)
+            {
+                if (ReferenceEquals(sr, null)) continue;
+                
+                //创建残影对象
+                // 创建残影对象
+                GameObject afterImage = new GameObject("AfterImage");
+                SpriteRenderer imageSr = afterImage.AddComponent<SpriteRenderer>();
 
+                // 复制 Sprite、颜色、排序层等属性
+                imageSr.sprite = sr.sprite;
+                imageSr.sortingLayerID = sr.sortingLayerID;
+                imageSr.sortingOrder = sr.sortingOrder - 1; // 稍微降低层级避免遮挡
+                imageSr.color = new Color(1, 1, 1, 0.5f); // 半透明效果
+
+                // 设置位置与旋转
+                afterImage.transform.position = sr.transform.position;
+                afterImage.transform.rotation = sr.transform.rotation;
+                afterImage.transform.localScale = sr.transform.lossyScale;
+
+                // 渐隐消失
+                imageSr.DOFade(0f, 0.5f).OnComplete(() => Object.Destroy(afterImage, 0.5f));
+            }
+        }
     }
 }

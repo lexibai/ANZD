4bceacfd5da53676151bd7b2bc142cf72c02c37d
花天酒地唐斜阳 <thle522@foxmail.com>
2025-06-26 22:26:41 +0800

    feat(技能): 新增技能类型

---
+++ Assets/Scripts/Skill/SkillType.cs
@@ -0,0 +1,34 @@
+namespace SkillModule
+{
+    /// <summary>
+    /// 技能类型
+    /// </summary>
+    public enum SkillType
+    {
+        /// <summary>
+        /// 普通攻击
+        /// </summary>
+        Atk,
+
+        /// <summary>
+        /// 普通射击
+        /// </summary>
+        Fire,
+        /// <summary>
+        /// 魔法技能
+        /// </summary>
+        Magic, 
+        /// <summary>
+        /// 武技
+        /// </summary>
+        MartialSkill,
+        /// <summary>
+        /// 位移技能
+        /// </summary>
+        Move, 
+        /// <summary>
+        /// 终结技
+        /// </summary>
+        Ultimeta, 
+    }
+}
